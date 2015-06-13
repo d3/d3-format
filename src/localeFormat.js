851ea7586b25718bd7fda877300c86582dd3c97b
Mike Bostock <mbostock@gmail.com>
2015-06-13 12:38:54 -0700

    Delete dead code.

--- src/localeFormat.js
+++ src/localeFormat.js
@@ -4,27 +4,6 @@ import btod from "./btod";
 var formatRe = /(?:([^{])?([<>=^]))?([+\- ])?([$#])?(0)?(\d+)?(,)?(\.-?\d+)?([a-z%])?/i,
     systemSymbols = ["y","z","a","f","p","n","µ","m","","k","M","G","T","P","E","Z","Y"];
 
-export default function(x, p) {
-  var i = (x = p == null ? x.toExponential() : x.toExponential(p - 1)).indexOf("e"); // TODO ternary?
-  if (i < 0) return x;
-
-  var coefficient = x.slice(0, i),
-      exponent = +x.slice(i + 1),
-      offset = exponent % 3;
-
-  if (offset) {
-    var j = coefficient.indexOf("."),
-        decimal = j < 0 ? coefficient : coefficient.slice(0, j),
-        fractional = j < 0 ? "" : coefficient.slice(j + 1);
-    if (offset < 0) offset += 3;
-    coefficient = decimal + (offset >= fractional.length
-        ? fractional + new Array(offset - fractional.length + 1).join("0")
-        : fractional.slice(0, offset) + "." + fractional.slice(offset));
-  }
-
-  return coefficient + symbols[8 + Math.floor(exponent / 3)];
-};
-
 var formatTypes = {
   "b": function(x) { return x.toString(2); },
   "c": function(x) { return String.fromCharCode(x); },
