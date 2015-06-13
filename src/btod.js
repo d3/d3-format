b515953e5354c66b85145fef87c973415566e6bc
Mike Bostock <mbostock@gmail.com>
2015-06-13 16:18:44 -0700

    Cleaner.

--- src/btod.js
+++ src/btod.js
@@ -1,15 +1,15 @@
 // Note: assumes that x is positive.
 export default function(x, p) {
-  var s = p == null ? x.toExponential() : x.toExponential(p - 1),
-      i = s.indexOf("e");
-  if (i < 0) return null; // NaN, ±Infinity
-  var coefficient = s.slice(0, i),
-      exponent = +s.slice(i + 1),
+  x = p == null ? x.toExponential() : x.toExponential(p - 1);
+  if ((i = x.indexOf("e")) < 0) return null; // NaN, ±Infinity
+  var i,
+      coefficient = x.slice(0, i),
+      exponent = +x.slice(i + 1),
       j = coefficient.indexOf("."),
-      decimal = j < 0 ? coefficient : coefficient.slice(0, j),
-      fractional = j < 0 ? "" : coefficient.slice(j + 1);
+      integer = j < 0 ? coefficient : coefficient.slice(0, j),
+      fraction = j < 0 ? "" : coefficient.slice(j + 1);
   return {
-    coefficient: decimal + fractional,
-    exponent: exponent + decimal.length
+    coefficient: integer + fraction,
+    exponent: exponent + integer.length
   };
 };
