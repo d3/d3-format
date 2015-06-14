92d3cadf33eadd768bb98ae865148d3d0c7c36c6
Mike Bostock <mbostock@gmail.com>
2015-06-13 21:42:41 -0700

    Update test.

--- test/formatPrefix-test.js
+++ test/formatPrefix-test.js
@@ -2,8 +2,9 @@ var tape = require("tape"),
     format = require("../");
 
 tape("formatPrefix(\"s\", prefix)(number) formats with the specified SI prefix", function(test) {
-  test.equal(format.formatPrefix(".0s", "µ")(.00042), "420µ");
-  test.equal(format.formatPrefix(".3s", "m")(.00042), "0.420m");
+  test.equal(format.formatPrefix(",.0s", "µ")(.00042), "420µ");
+  test.equal(format.formatPrefix(",.0s", "µ")(.0042), "4,200µ");
+  test.equal(format.formatPrefix(",.3s", "m")(.00042), "0.420m");
   test.end();
 });
 
