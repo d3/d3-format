cb6d1e7ff057f02727f28bb2f27603fd7f715db2
Mike Bostock <mbostock@gmail.com>
2015-06-15 14:07:58 -0700

    Fix comment.

--- src/formatDecimal.js
+++ src/formatDecimal.js
@@ -1,6 +1,6 @@
 // Computes the decimal coefficient and exponent of the specified number x with
 // significant digits p, where x is positive and p is in [1, 21] or undefined.
-// For example, formatDecimal(1.23) returns ["123", -1].
+// For example, formatDecimal(1.23) returns ["123", 0].
 export default function(x, p) {
   if ((i = (x = x.toExponential(p && p - 1)).indexOf("e")) < 0) return null; // NaN, ±Infinity
   var i, coefficient = x.slice(0, i);
