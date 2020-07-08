03ffad22748b1efd038ca4e9f031e97deb85693b
Philippe Rivière <fil@rezo.net>
2020-07-08 18:07:03 +0200

    Update src/index.js
    Co-authored-by: Curran Kelleher <curran.kelleher@gmail.com>

--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
 export {default as currencyPrecisionPrefix} from "./currencyPrecisionPrefix.js";
-export { default as formatDefaultLocale, format, formatCurrencyPrefix, formatPrefix} from "./defaultLocale.js";
+export {default as formatDefaultLocale, format, formatCurrencyPrefix, formatPrefix} from "./defaultLocale.js";
 export {default as formatLocale} from "./locale.js";
 export {default as formatSpecifier, FormatSpecifier} from "./formatSpecifier.js";
 export {default as precisionFixed} from "./precisionFixed.js";
