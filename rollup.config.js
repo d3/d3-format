5f436cc7809de70ff72e80c4971e1ffe9e2ade07
Mike Bostock <mbostock@gmail.com>
2021-04-16 13:16:16 -0700

    restore unminified bundle

--- rollup.config.js
+++ rollup.config.js
@@ -1,25 +1,34 @@
 import {terser} from "rollup-plugin-terser";
 import * as meta from "./package.json";
 
-const copyright = `// ${meta.homepage} v${meta.version} Copyright ${(new Date).getFullYear()} ${meta.author.name}`;
+const config = {
+  input: "src/index.js",
+  external: Object.keys(meta.dependencies || {}).filter(key => /^d3-/.test(key)),
+  output: {
+    file: `dist/${meta.name}.js`,
+    name: "d3",
+    format: "umd",
+    indent: false,
+    extend: true,
+    banner: `// ${meta.homepage} v${meta.version} Copyright ${(new Date).getFullYear()} ${meta.author.name}`,
+    globals: Object.assign({}, ...Object.keys(meta.dependencies || {}).filter(key => /^d3-/.test(key)).map(key => ({[key]: "d3"})))
+  },
+  plugins: []
+};
 
 export default [
+  config,
   {
-    input: "src/index.js",
-    external: Object.keys(meta.dependencies || {}).filter(key => /^d3-/.test(key)),
+    ...config,
     output: {
-      file: `dist/${meta.name}.min.js`,
-      name: "d3",
-      format: "umd",
-      indent: false,
-      extend: true,
-      banner: copyright,
-      globals: Object.assign({}, ...Object.keys(meta.dependencies || {}).filter(key => /^d3-/.test(key)).map(key => ({[key]: "d3"})))
+      ...config.output,
+      file: `dist/${meta.name}.min.js`
     },
     plugins: [
+      ...config.plugins,
       terser({
         output: {
-          preamble: copyright
+          preamble: config.output.banner
         }
       })
     ]
